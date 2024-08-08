d57bc17124d7dc8a9d3db48fe51ffffdd8f6172b
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2024-08-08 01:52:04 -0300

    criação de No

---
+++ src/No.java
@@ -0,0 +1,14 @@
+package GerenciamentodeOSs;
+
+public class No {
+    int alt;
+    No esq, dir;
+    OrdensdeServicos os;
+
+    public No(OrdensdeServicos data) {
+        this.os = data;
+        this.alt = 0;
+        this.esq = null;
+        this.dir = null;
+    }
+}
