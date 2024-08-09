368eafdf957e20c70333d6ea6f66023435badf64
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2024-08-08 23:36:47 -0300

    arquivo de log e criação de main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+package GerenciamentodeOSs;
+
+public class Main {
+    public static void main(String[] args) {
+        Servidor servidor = new Servidor();
+        Cliente cliente = new Cliente(servidor);
+        ClienteHandler clienteHandler = new ClienteHandler(cliente);
+        try {
+			clienteHandler.iniciar();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
