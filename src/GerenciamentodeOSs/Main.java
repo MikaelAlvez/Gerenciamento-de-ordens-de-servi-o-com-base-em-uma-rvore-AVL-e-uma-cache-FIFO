ecea414b5b2d120cb37b4b64c1da93787466751e
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2024-08-09 23:18:11 -0300

    Atualização de dados em arquivo

---
+++ src/GerenciamentodeOSs/Main.java
@@ -0,0 +1,19 @@
+package GerenciamentodeOSs;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Servidor servidor = new Servidor();
+            servidor.carregarDadosIniciais("BancodeDados.txt");
+
+            Cliente cliente = new Cliente(servidor);
+
+            ClienteHandler clienteHandler = new ClienteHandler(cliente);
+
+            clienteHandler.iniciar();
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+}
--- src/GerenciamentodeOSs/Servidor.java
+++ src/GerenciamentodeOSs/Servidor.java
@@ -1,9 +1,13 @@
 package GerenciamentodeOSs;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -18,6 +22,32 @@ public class Servidor {
     this.cache = new Cache();
     this.hits = 0;
     this.miss = 0;
+    
+    carregarDadosIniciais("BancodeDados.txt");
+  }
+  
+  public void carregarDadosIniciais(String fileName) {
+      try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+          String linha;
+          DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+          while ((linha = br.readLine()) != null) {
+              String[] dados = linha.split(","); // Assume que o delimitador é vírgula
+              int codigo = Integer.parseInt(dados[0]);
+              String nome = dados[1];
+              String descricao = dados[2];
+              LocalDateTime horario = LocalDateTime.parse(dados[3], formatter); // Parse do horário
+
+              OrdensdeServicos os = new OrdensdeServicos(codigo, nome, descricao);
+              os.setHorario(horario); // Define o horário lido do arquivo
+              BancodeDados.adicionar(os);  // Adiciona a OS à árvore AVL
+          }
+          System.out.println("Banco de Dados carregado com sucesso!");
+      } catch (IOException e) {
+          System.out.println("Erro ao carregar os dados iniciais: " + e.getMessage());
+      } catch (Exception e) {
+          System.out.println("Erro ao adicionar ordens de serviço: " + e.getMessage());
+      }
   }
 
   public OrdensdeServicos buscarOSCache(int cod) throws Exception{
@@ -46,6 +76,8 @@ public class Servidor {
     } else {
     	log("Insercao", false, alturaAnt, BancodeDados.getAltura(), os.getCod());
     }
+    
+    atualizarArquivo();
 
     return true;
   }
@@ -63,6 +95,8 @@ public class Servidor {
       cache.remover(ordem);
       cache.fifo(os);
     }
+    
+    atualizarArquivo();
 
     return true;
   }
@@ -83,9 +117,32 @@ public class Servidor {
     } else {
     	log("Remocao", false, alturaAnt, BancodeDados.getAltura(), cod);
     }
+    
+    atualizarArquivo();
 
     return true;
   }
+  
+  private void atualizarArquivo() {
+	    try (BufferedWriter writer = new BufferedWriter(new FileWriter("BancodeDados.txt"))) {
+	        List<OrdensdeServicos> listaOS = BancodeDados.listaOS();
+	        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+	        for (OrdensdeServicos os : listaOS) {
+	            String linha = String.format("%d,%s,%s,%s",
+	                os.getCod(),
+	                os.getNome(),
+	                os.getDescricao(),
+	                os.getHorario().format(formatter)
+	            );
+	            writer.write(linha);
+	            writer.newLine();
+	        }
+	    } catch (IOException e) {
+	        System.out.println("Erro ao atualizar o arquivo de banco de dados: " + e.getMessage());
+	    }
+	}
+
 
   public int getRegistros() {
     return BancodeDados.getRegistros();
