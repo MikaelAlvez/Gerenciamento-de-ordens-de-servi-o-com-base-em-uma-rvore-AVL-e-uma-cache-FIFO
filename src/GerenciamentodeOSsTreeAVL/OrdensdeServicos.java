5c410516c202f9431ebde14dcc9de23b5dee08dc
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2024-10-22 21:04:39 -0300

    correction: add Pratica 1 ao projeto

---
+++ src/GerenciamentodeOSsTreeAVL/OrdensdeServicos.java
@@ -0,0 +1,54 @@
+package GerenciamentodeOSsTreeAVL;
+
+import java.time.LocalDateTime;
+
+public class OrdensdeServicos {
+  private final int cod;
+  private String nome;
+  private String descricao;
+  private LocalDateTime horario;
+
+  private static int cont = 0;
+  
+  public OrdensdeServicos(Integer codigo, String nome, String descricao) {
+    if (codigo != null) {
+      if (codigo > cont) {
+        cont = codigo;
+      }
+      this.cod = codigo;
+    } else {
+      this.cod = cont++;
+    }
+    this.nome = nome;
+    this.descricao = descricao;
+    this.horario = LocalDateTime.now();
+  }
+
+  public int getCod() {
+    return cod;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public LocalDateTime getHorario() {
+    return horario;
+  }
+
+  public void setHorario(LocalDateTime horario) {
+    this.horario = horario;
+  }
+}
